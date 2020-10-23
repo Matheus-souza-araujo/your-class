026841a7b74b9513b0a5b0afced8b347410258f7
Matheus-souza-araujo <matheus1mrs@gmail.com>
2020-10-22 23:27:05 -0300

    Menu ativo feito, resta formulário

--- routes.js
+++ routes.js
@@ -13,7 +13,6 @@ routes.get('/teachers/create', function(req,res){
 })
 routes.get('/teachers/:id', teachers.show)
 routes.get('/teachers/:id/edit', teachers.edit)
-routes.post('/teachers', teachers.post)
 routes.put('/teachers', teachers.put)
 routes.delete('/teachers', teachers.delete)
 routes.get('/members', function(res, res){ //criando a rota
